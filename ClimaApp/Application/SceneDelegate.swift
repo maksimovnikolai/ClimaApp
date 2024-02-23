c09ff674fed858cf6830a107eb37d7f207589e02
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-23 21:46:32 +0300

    setup project

---
+++ ClimaApp/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  ClimaApp
+//
+//  Created by Nikolai Maksimov on 23.02.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = WeatherViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ ClimaApp/Controllers/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.swift
+//  ClimaApp
+//
+//  Created by Nikolai Maksimov on 23.02.2024.
+//
+
+import UIKit
+
+final class WeatherViewController: UIViewController {
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
